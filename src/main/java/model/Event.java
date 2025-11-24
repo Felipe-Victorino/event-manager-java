fbce29fd4921f4d7d8234641f12c6330f0f39a6c
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-11-24 18:41:20 -0300

    fix(Event.java): class did not exist, oops

---
+++ src/main/java/model/Event.java
@@ -0,0 +1,65 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+@Table(name = "event")
+public class Event {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "event_id")
+	long id;
+
+	String name;
+
+	@Temporal(TemporalType.DATE)
+	Date startDate;
+	@Temporal(TemporalType.DATE)
+	Date endDate;
+
+
+	@Override
+	public String toString(){
+		return getName() + '\n'
+				+ getEndDate() + '\n'
+				+ getStartDate() + '\n';
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+
+}
