26667152b604310b161e440a5fefad573c622013
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-11-26 12:11:17 -0300

    fix(Person): By mistake, the abstract class was not marked an an entity, w hich caused the child classes to not inherit attributes in the database

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,10 +1,21 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
 
+@Entity
 @Inheritance (strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Person {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	protected long id;
+
 	protected String name;
 	protected String cpf;
 
