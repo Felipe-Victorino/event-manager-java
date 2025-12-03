0176e03ce494462df1e26278f00351373fd9a38f
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-12-03 15:01:49 -0300

    fix(persistence): persistence now won't print every little thing, also created test classes, and parts of the system seem to be working

--- src/test/java/BasicData.java
+++ src/test/java/BasicData.java
@@ -1,19 +1,52 @@
 import com.github.javafaker.Faker;
 import model.Participant;
+import service.EventService;
 import service.ParticipantService;
 import service.RoomService;
 import service.SpeakerService;
 
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Random;
 
 public class BasicData {
 
+	//Perguntar como fazer a população da base de dados sem o limite de conexões
+
 	public static void main() {
+		System.out.println("Test class");
 
 
 
+	}
+
+	public static void genEvent(){
+		Faker faker = new Faker(new Locale("pt-BR"));
+		Date start = new Date(2025-1900, Calendar.JANUARY, 1);
+		//para inserir o date dessa forma tem que se tirar 1900 do ano
+		Calendar c= Calendar.getInstance();
+		c.setTime(start);
+		c.add(Calendar.DATE, 30);
+		Date end = c.getTime();
+		new EventService().createEvent(faker.job().field(), start, end);
+	}
+
+	public static Date randomDate(int min, int max){ //função quebrada
+		int minday = (int) new Date(min -1900, Calendar.JANUARY, 1).getTime();
+		int maxday = (int) new Date(max -1900, Calendar.DECEMBER, 1).getTime();
+		long randomDay = minday + new Random().nextInt(maxday - minday);
+
+		return new Date(randomDay);
+	}
+
+	public static Date randomDateBeforeNow(){
+		return new Date(new Date().getTime() - new Random().nextInt());
+	}
+
+	public static Date randomDateAfterNow(){
+		return new Date(new Date().getTime() + new Random().nextInt());
 	}
 
 	public static String cpfGen(){
@@ -28,26 +61,18 @@ public class BasicData {
 	}
 
 	public static void genSpeaker(){
-		try {
-			for (int i = 0; i < 100; i++) {
-				new SpeakerService().createSpeaker(nameGen(), cpfGen());
-				Thread.sleep(500L);
-			}
-		} catch (InterruptedException e){
-			System.out.println(e.getMessage());
-			System.out.println(Arrays.toString(e.getStackTrace()));
+		SpeakerService server = new SpeakerService();
+		for (int i = 0; i < 100; i++) {
+			server.createSpeaker(nameGen(), cpfGen());
 		}
+
+
 	}
 
 	public static void genParticipant(){
-		try {
-			for (int i = 0; i < 100; i++) {
-				new ParticipantService().createParticipant(nameGen(), cpfGen());
-				Thread.sleep(500L);
-			}
-		} catch (InterruptedException e){
-			System.out.println(e.getMessage());
-			System.out.println(Arrays.toString(e.getStackTrace()));
+		ParticipantService server = new ParticipantService();
+		for (int i = 0; i < 1000; i++) {
+			server.createParticipant(nameGen(), cpfGen());
 		}
 	}
 
